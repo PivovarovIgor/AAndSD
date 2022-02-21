8ecdc852ae575810f8ffb493ddb6250bd030e197
PivIgor <brauer.igor@gmail.com>
2022-02-21 20:02:11 +0300

    Init commit of lesson 2.

---
+++ src/main/lesson2/Main.java
@@ -0,0 +1,39 @@
+package lesson2;
+
+import lesson2.sort.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArray.isRepeat = false;
+        MyArray.isShuffle = false;
+        final int SIZE = 50000;
+        Integer[] arr = MyArray.getArray(SIZE, 2);
+
+        List<Integer> list = new ArrayList<>();
+        Collections.addAll(list, arr);
+
+//        System.out.println(Arrays.toString(arr));
+
+        SpeedTest.startTime();
+
+//        BubbleSort.sort(arr); //при 50000 - 11704 / 3
+//        CocktailSort.sort(arr); //12185
+//        CombSort.sort(arr); // 42
+//        SelectionSort.sort(arr); //3135 / 983
+        InsertionSort.sort(arr); // 2310 / 6
+//        CountingSort.sort(arr); // 12
+//        Arrays.sort(arr); // 33
+//        Collections.sort(list); // 33
+
+
+        SpeedTest.endTime();
+
+//        System.out.println(Arrays.toString(arr));
+
+    }
+}
---
+++ src/main/lesson2/search/ExponentialSearch.java
@@ -0,0 +1,8 @@
+package lesson2.search;
+
+public class ExponentialSearch {
+
+    public static int search(Integer[] arr, Integer num2Find) {
+        return -1;
+    }
+}
